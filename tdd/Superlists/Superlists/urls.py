03d311c5aff4ab395f54fbf9442bd4834ff222cf
alyneperez <alyne.kmp@gmail.com>
2022-10-07 15:13:08 -0300

    feat: basic view now returns minimal HTML

--- tdd/Superlists/Superlists/urls.py
+++ tdd/Superlists/Superlists/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from lists import views
 
+
 urlpatterns = [
-    url(r'^$', views.home_page, name="home"),
+    url(r"^$", views.home_page, name="home"),
 ]
