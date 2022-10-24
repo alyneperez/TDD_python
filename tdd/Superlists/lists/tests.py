c8ed226f849c26fd099fd9e53217b794b8a29481
alyneperez <alyne.kmp@gmail.com>
2022-10-24 16:33:00 -0300

    test: redirect after POST, and show all items in template

--- tdd/Superlists/lists/tests.py
+++ tdd/Superlists/lists/tests.py
@@ -8,16 +8,44 @@ from lists.views import home_page
 from lists.models import Item
 
 
+# ToDo:
+# Não salvar itens em branco a cada requisição ✓
+# Code smell: teste de POST é longo demais? ✓
+# Exibir vários itens da tabela
+# Aceitar mais de uma lista
+
+
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "lists/home.html")
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
+
+    def test_only_saves_item_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, "lists/home.html")
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+        
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item "})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -36,13 +64,3 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
-
-    # def test_home_page_returns_correct_html(self):
-    #     response = self.client.get("/")
-
-    #     html = response.content.decode("utf8")
-    #     self.assertTrue(html.startswith("<html>"))
-    #     self.assertIn("<title>To-Do lists</title>", html)
-    #     self.assertTrue(html.strip().endswith("</html>"))
-
-    #     self.assertTemplateUsed(response, "lists/home.html")
